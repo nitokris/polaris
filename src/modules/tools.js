92fff44f7c6f2004404b4694065a815d9362f29c
nitokris <nitokrisalpha@outlook.com>
2020-11-17 18:57:39 +0800

    进度条

--- src/modules/tools.js
+++ src/modules/tools.js
@@ -10,23 +10,7 @@ export function progressBar() {
                 result = parseInt(s / (a - b) * 100),
                 cached = $("#progressBar");
             cached.css("width", result + "%");
-            if (false) {
-                if (result >= 0 && result <= 19)
-                    cached.css("background", "#cccccc");
-                if (result >= 20 && result <= 39)
-                    cached.css("background", "#50bcb6");
-                if (result >= 40 && result <= 59)
-                    cached.css("background", "#85c440");
-                if (result >= 60 && result <= 79)
-                    cached.css("background", "#f2b63c");
-                if (result >= 80 && result <= 99)
-                    cached.css("background", "#FF0000");
-                if (result == 100)
-                    cached.css("background", "#5aaadb");
-            } else {
-                cached.css("background", "orange");
-            }
-
+            cached.css("background", "orange");
         });
     }
 }
